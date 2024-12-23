caba66c7bf6a708938795dc3bb18089241b887af
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-12-24 00:21:57 +0400

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,23 +1,36 @@
 def custom_write(file_name, strings):
+    file_tell = []
+    file_num = []
+    file_line = []
     file = open(file_name, 'a', encoding='utf-8')
     for info_single in info:
+        file_tell.append(file.tell())
         file.write(f'{info_single}\n')
     file.close()
+    with open(file_name, 'r', encoding='utf-8') as file:
+        for num, line in enumerate(file, start=1):
+            file_num.append(num)
+            file_line.append(line)
+    print(file_tell)
+    print(file_num)
+    print(file_line)
+
+
 
 info = ['Text for tell.', 'Используйте кодировку utf-8.', 'Because there are 2 languages!', 'Спасибо!']
 result = custom_write('test.txt', info)
 
 
 
-
-# Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>),
-# а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell() перед записью.
-users_tuple = ((("+111", "123455"), "Tom"), ("+384767557", "Bob"), ("+958758767", "Alice"))
-users_dict = dict(users_tuple)
-print(users_dict)
-
-users_list = [["+111123455", "Tom"], ["+384767557", "Bob"], ["+958758767", "Alice"]]
-users_dict = dict(users_list)
-print(users_dict)
+#
+# # Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>),
+# # а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell() перед записью.
+# users_tuple = ((("+111", "123455"), "Tom"), ("+384767557", "Bob"), ("+958758767", "Alice"))
+# users_dict = dict(users_tuple)
+# print(users_dict)
+#
+# users_list = [["+111123455", "Tom"], ["+384767557", "Bob"], ["+958758767", "Alice"]]
+# users_dict = dict(users_list)
+# print(users_dict)
 
 # Пример полученного словаря: {(1, 0): 'Text for tell.', (2, 16): 'Используйте кодировку utf-8.'}
